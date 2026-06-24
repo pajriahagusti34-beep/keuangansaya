384188fe04c2983234aa7afda674f1ec9cbef2ab
pajriahagusti34-beep <pajriahagusti34@gmail.com>
2026-06-24 22:25:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,82 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(page_title="Keuangan Pribadi")
+st.set_page_config(page_title="Keuangan Pribadi", page_icon="💰")
 
 st.title("💰 Aplikasi Keuangan Pribadi")
 
-if "saldo" not in st.session_state:
-    st.session_state.saldo = 0
+if "data" not in st.session_state:
+    st.session_state.data = []
 
-if "riwayat" not in st.session_state:
-    st.session_state.riwayat = []
+with st.form("form_keuangan"):
+    jenis = st.selectbox(
+        "Jenis Transaksi",
+        ["Pemasukan", "Pengeluaran"]
+    )
 
-menu = st.selectbox(
-    "Pilih Transaksi",
-    ["Pemasukan", "Pengeluaran"]
-)
+    keterangan = st.text_input("Keterangan")
 
-keterangan = st.text_input("Keterangan")
-jumlah = st.number_input("Jumlah", min_value=0)
+    nominal = st.number_input(
+        "Nominal",
+        min_value=0
+    )
 
-if st.button("Simpan"):
-    if menu == "Pemasukan":
-        st.session_state.saldo += jumlah
-        st.session_state.riwayat.append(
-            f"Pemasukan - {keterangan} : Rp {jumlah:,.0f}"
+    submit = st.form_submit_button("Simpan")
+
+    if submit:
+        st.session_state.data.append({
+            "Jenis": jenis,
+            "Keterangan": keterangan,
+            "Nominal": nominal
+        })
+
+df = pd.DataFrame(st.session_state.data)
+
+if not df.empty:
+
+    total_pemasukan = df[df["Jenis"]=="Pemasukan"]["Nominal"].sum()
+
+    total_pengeluaran = df[df["Jenis"]=="Pengeluaran"]["Nominal"].sum()
+
+    saldo = total_pemasukan - total_pengeluaran
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric(
+            "Total Pemasukan",
+            f"Rp {total_pemasukan:,.0f}"
         )
-    else:
-        st.session_state.saldo -= jumlah
-        st.session_state.riwayat.append(
-            f"Pengeluaran - {keterangan} : Rp {jumlah:,.0f}"
+
+    with col2:
+        st.metric(
+            "Total Pengeluaran",
+            f"Rp {total_pengeluaran:,.0f}"
         )
 
-    st.success("Data berhasil disimpan")
+    with col3:
+        st.metric(
+            "Saldo Saat Ini",
+            f"Rp {saldo:,.0f}"
+        )
 
-st.subheader("Saldo Saat Ini")
-st.write(f"Rp {st.session_state.saldo:,.0f}")
+    st.subheader("📋 Riwayat Transaksi")
+    st.dataframe(df, use_container_width=True)
 
-st.subheader("Riwayat Transaksi")
+    st.subheader("📊 Grafik Keuangan")
 
-for item in st.session_state.riwayat:
-    st.write(item)
+    grafik = pd.DataFrame({
+        "Jumlah": [
+            total_pemasukan,
+            total_pengeluaran
+        ]
+    },
+    index=[
+        "Pemasukan",
+        "Pengeluaran"
+    ])
+
+    st.bar_chart(grafik)
+
+else:
+    st.info("Belum ada transaksi.")
